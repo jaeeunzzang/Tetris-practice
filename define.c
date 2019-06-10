e2f4dca08b858f62b6e0b2d566cf56cf119fa375
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-06-10 11:32:22 +0900

    Update define.c

--- define.c
+++ define.c
@@ -7,6 +7,11 @@
 #define RIGHT 77
 #define SPACEBAR 32
 
+typedef struct _COORD{
+  short x;
+  short y;
+}COORD;
+
 
 typedef struct _tetris{
   int level;
@@ -17,12 +22,48 @@ typedef struct _tetris{
   int nextblock;
   int absX,absY;
   int curX,curY;
-  }Tetris;
+ }Tetris;
   
   char key;
   key=getch();
   
+while(kbhit()) {
+     key=getch();
+     switch(key) {
+      
+        case UP;
+        break;
+      
+        case DOWN;
+        break;
+      
+        case LEFT;
+        break;
+       
+        case RIGHT;
+        break;
+      
+               }
   
+  if(key==SPACEBAR) {
+    /* later
+    */
+    
+             }
+  }
+  
+void gotoxy(int x,int y){
+  COORD pos={x,y};
+  SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),pos);
+  }
+  
+void removeCur(){
+  CONSOLE_CURSOR_INFO curinfo;
+  curinfo.bVisible=0;
+  GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&curinfo);
+  }
+
+
 
 
 
