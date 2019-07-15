1a3b5d548429fb8841271e0bcf72d345daa7d764
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-15 11:29:45 +0900

    Update define.c

--- define.c
+++ define.c
@@ -277,7 +277,7 @@ void title(void)
   goto(x,y+2);  printf("│                   │"); Sleep(100);
   goto(x,y+3);  printf("│                   │"); Sleep(100);
   goto(x,y+4);  printf("└───────────────────┘"); Sleep(100);
-  goto(x,y+5);  printf("Please Enter Any Key to Start");
+  goto(x,y+5);  printf("Press Any Key to Start");
   
   for(cnt=0;;cnt++)
   {
@@ -842,5 +842,46 @@ void title(void)
        }
      }
    }
-         
-     
+ 
+    void pause(void)
+    {
+      int i,j;
+      int x=5;
+      int y=5;
+      
+      for(i=1;i<MAIN_X-2;i++)
+      {
+         gotoxy(x,y+0); printf("▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤");
+         gotoxy(x,y+1); printf("▤                           ▤");
+         gotoxy(x,y+2); printf("▤ +-----------------------+ ▤");
+         gotoxy(x,y+3); printf("▤ |      P A U S E . .    | ▤");
+         gotoxy(x,y+4); printf("▤ +-----------------------+ ▤");
+         gotoxy(x,y+5); printf("▤ Press Any Key To Resume.. ▤");
+         gotoxy(x,y+6); printf("▤                           ▤");
+         gotoxy(x,y+7); printf("▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤▤");
+      }
+      getch();
+      
+      system("cls"); //화면 지우고 새로 그림
+      reset_main_cpy();
+      draw_main();
+      draw_map();
+      
+      for(i=1;i<3;i++)
+      {
+        for(j=0;j<4;j++)
+        {
+          if(blocks[b_type_next][0][i][j]==1)
+          {
+            gotoxy(MAIN_X+MAIN_X_ADJ+3+j,i+6);
+            printf("■");
+          }
+          else
+          {
+            gotoxy(MAIN_X+MAIN_X_ADJ+3+j,i+6);
+            printf(" ");
+          }
+        }
+      }
+    }
+      
